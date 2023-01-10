81e5c96280fd1a3698f034decb3a489021f9d8c5
Jayakrishnan-manoj <91311543+Jayakrishnan-manoj@users.noreply.github.com>
2023-01-10 19:39:31 +0530

    minor code refactor

--- lib/screens/auth/sign_up.dart
+++ lib/screens/auth/sign_up.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:task_master/screens/auth/sign_in.dart';
 import 'package:task_master/screens/tasks/task_screen.dart';
 import 'package:task_master/services/shared_functions.dart';
@@ -24,161 +25,171 @@ class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 30,
-            bottom: 40,
-            left: 20,
-            right: 20,
-          ),
-          child: Form(
-            key: FormKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  "TaskMaster",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                  ),
+      body: _isLoading
+          ? Center(
+              child: SpinKitRipple(
+                color: Theme.of(context).primaryColor,
+                size: 100,
+              ),
+            )
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 30,
+                  bottom: 40,
+                  left: 20,
+                  right: 20,
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text(
-                  "Create an account and manage tasks the right way!",
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Image.asset('assets/to-do.png'),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  decoration: textInputDecoration.copyWith(
-                    label: const Text("FullName"),
-                    prefixIcon: Icon(
-                      Icons.person,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
-                  onChanged: (value) => setState(() {
-                    fullName = value;
-                  }),
-                  validator: (value) {
-                    if (value!.isNotEmpty) {
-                      return null;
-                    } else {
-                      return "Name cannot be empty";
-                    }
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  decoration: textInputDecoration.copyWith(
-                    label: const Text("Email"),
-                    prefixIcon: Icon(
-                      Icons.email,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
-                  onChanged: (value) => setState(() {
-                    email = value;
-                  }),
-                  validator: (value) {
-                    return RegExp(
-                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                            .hasMatch(value!)
-                        ? null
-                        : "Please enter a valid email";
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: textInputDecoration.copyWith(
-                    label: const Text("password"),
-                    prefixIcon: Icon(
-                      Icons.key,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
-                  onChanged: (value) {
-                    setState(() {
-                      password = value;
-                    });
-                  },
-                  validator: (value) {
-                    if (value!.length > 8) {
-                      return null;
-                    } else {
-                      return "password length must be 8 or greater";
-                    }
-                  },
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 15.0),
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        elevation: 0,
-                        backgroundColor: Colors.indigo,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        )),
-                    onPressed:()=> auth.signUpUser(fullName, email, password),
-                    child: const Text(
-                      "Sign Up",
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-                Text.rich(
-                  TextSpan(
-                    text: "Already have an account?",
-                    style: const TextStyle(color: Colors.black, fontSize: 14),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: " Sign in",
+                child: Form(
+                  key: FormKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "TaskMaster",
                         style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          decoration: TextDecoration.underline,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
                         ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () => nextScreenReplace(
-                                context,
-                                const SignUpScreen(),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        "Create an account and manage tasks the right way!",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w400),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Image.asset('assets/to-do.png'),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        decoration: textInputDecoration.copyWith(
+                          label: const Text("FullName"),
+                          prefixIcon: Icon(
+                            Icons.person,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                        onChanged: (value) => setState(() {
+                          fullName = value;
+                        }),
+                        validator: (value) {
+                          if (value!.isNotEmpty) {
+                            return null;
+                          } else {
+                            return "Name cannot be empty";
+                          }
+                        },
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        decoration: textInputDecoration.copyWith(
+                          label: const Text("Email"),
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                        onChanged: (value) => setState(() {
+                          email = value;
+                        }),
+                        validator: (value) {
+                          return RegExp(
+                                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                  .hasMatch(value!)
+                              ? null
+                              : "Please enter a valid email";
+                        },
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: textInputDecoration.copyWith(
+                          label: const Text("password"),
+                          prefixIcon: Icon(
+                            Icons.key,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            password = value;
+                          });
+                        },
+                        validator: (value) {
+                          if (value!.length > 8) {
+                            return null;
+                          } else {
+                            return "password length must be 8 or greater";
+                          }
+                        },
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 15.0),
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              backgroundColor: Colors.indigo,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              )),
+                          onPressed: () =>
+                              auth.signUpUser(fullName, email, password),
+                          child: const Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Text.rich(
+                        TextSpan(
+                          text: "Already have an account?",
+                          style: const TextStyle(
+                              color: Colors.black, fontSize: 14),
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: " Sign in",
+                              style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                decoration: TextDecoration.underline,
                               ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () => nextScreenReplace(
+                                      context,
+                                      const SignUpScreen(),
+                                    ),
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
     );
   }
 
   signup() async {
-    if(FormKey.currentState!.validate()){
+    if (FormKey.currentState!.validate()) {
       setState(() {
         _isLoading = true;
       });
-      await auth.signUpUser(fullName, email, password).then((value) async{
-        if(value == true){
+      await auth.signUpUser(fullName, email, password).then((value) async {
+        if (value == true) {
           await HelperFunctions.saveUserEmail(email);
           await HelperFunctions.saveUserLoggedInStatus(true);
           await HelperFunctions.saveUserName(fullName);
@@ -191,6 +202,4 @@ class _SignUpScreenState extends State<SignUpScreen> {
       });
     }
   }
-
-
 }
